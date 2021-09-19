16e96996cd58ea9ac76f7e37d2011a6842e0cf3b
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-09-19 11:23:48 +0530

    Create plk.js

---
+++ plk.js
@@ -0,0 +1,27 @@
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+
+
+       
+    var r_text = new Array ();    
+r_text[0] = "98562";
+r_text[1] = "Pink32";
+r_text[2] = "deadman85";
+r_text[3] = "plk876pink";
+r_text[4] = "ripperrevi369";
+r_text[5] = "pallikkel@5";
+r_text[6] = "dudepoison";
+r_text[7] = "diedudepass";
+r_text[8] = "scenepink";
+r_text[9] = "godfath43";
+r_text[10] = "d34dm4n65";
+r_text[11] = "j0ck3rme";    
+var i = Math.floor(12*Math.random())
+
+var toxic = 'r_text[i]'
+
+
+
+
