73db102389a9794ad3790d80c6c562f09fda4be6
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-10-26 09:38:38 +0530

    Update plk.js

--- plk.js
+++ plk.js
@@ -1,11 +1,3 @@
 var anything = new RegExp(' ')
 
-  var plk_say = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
-  const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  var plk_here = new Date().toLocaleDateString(get_localized_date)
-
-                                                                                                                                                                                                                                                                                                module.exports = {
-                                                                                                                                                                                                                                                                                                               anything: anything,
-                                                                                                                                                                                                                                                                                                               plk_say: plk_say,
-                                                                                                                                                                                                                                                                                                               plk_here: plk_here
-                                                                                                                                                                                                                                                                                                               }
+                                                                                                                                                                                                                                                                                                module.exports = anything;
