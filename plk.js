ad801bd1b9b7f7ef91c4da306a7dd3dc205a17db
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-10-26 09:03:45 +0530

    Create plk.js

---
+++ plk.js
@@ -0,0 +1,11 @@
+var anything = new RegExp(' ')
+
+  var plk_say = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
+  const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  var plk_here = new Date().toLocaleDateString(get_localized_date)
+
+                                                                                                                                                                                                                                                                                                module.exports = {
+                                                                                                                                                                                                                                                                                                               anything: anything,
+                                                                                                                                                                                                                                                                                                               plk_time: plk_say,
+                                                                                                                                                                                                                                                                                                               plk_date: plk_here
+                                                                                                                                                                                                                                                                                                               }
